fa8b812f4682da0474715847145dc03754724a2f
Drishti-py <drishtichandpa@gmail.com>
2025-04-13 04:14:19 +0530

    Add sign in an login fixes with firestore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { getDatabase } from 'firebase/database';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 import { app } from "./firebase";
+import SignupPage from './Pages/Signup';
+import LoginPage from './Pages/Login';
 import './App.css';
 
-const db = getDatabase(app);
+const auth = getAuth(app);
 function App() {
+    const signupUser = () => {
+        createUserWithEmailAndPassword(auth,
+            "drishtichandpa.cute@gmail.com",
+            "Drishticutie"
+        ).then((value) => console.log(value));
 
+    };
+
+    return (
+        <div className='App'>
+            <SignupPage />
+            <LoginPage />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { app } from "../firebase";
+const auth = getAuth(app);
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const loginUser = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(value => console.log('Login success'))
+            .catch(err => console.log(err));
+    };
+    return (
+        <div className="login_page">
+            <h1>Log In</h1>
+            <label>Enter your email</label>
+            <input onChange={(e) => setEmail(e.target.value)} value={email} type='email' required placeholder='Enter your email'></input>
+            <label>Enter your password</label>
+            <input onChange={(e) => setPassword(e.target.value)} value={password} type='password' required placeholder='Enter your password'></input>
+            <button onClick={loginUser}>Log in</button>
+
+        </div>
+    );
+};
+export default LoginPage;
