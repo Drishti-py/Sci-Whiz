26b126c9b4543d40687c63df8ce600d4b31b12a1
Drishti-py <drishtichandpa@gmail.com>
2025-04-15 00:05:17 +0530

    Designing on CSS file for dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -5,16 +5,44 @@ function Dashboard() {
     return (
         <div className='dash'>
             <nav className='head'>
-                <img src='https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-2bd8-622f-8fde-0b982ba33e2e/raw?se=2025-04-13T18%3A36%3A03Z&sp=r&sv=2024-08-04&sr=b&scid=0f04d3a5-39ab-5ea5-b062-b90d6940e332&skoid=de76bc29-7017-43d4-8d90-7a49512bae0f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-13T04%3A49%3A38Z&ske=2025-04-14T04%3A49%3A38Z&sks=b&skv=2024-08-04&sig=QDG1TFmYGaAp%2BlHQg5CWWSB8WiM2xhftjvmgdut9wOk%3D' className='logo' ></img>
+                <img src='' className='logo' ></img>
             </nav>
-            <img src="https://cdn.dribbble.com/userupload/20060762/file/original-89e23399b461e9274e9f2f9acacc5ee7.gif" alt="funny gif" />
 
-            {/* <div className='text'></div> */}
             <div>
-                <div className='b1' id='back'> </div>
-                <div className='b2' id='back'> </div>
-                <div className='b3' id='back'> </div>
-                <div className='b4' id='back'> </div>
+                <div id='back'>
+                    <video
+                        className="video_len"
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                    >
+                        <source src="my_video1.mp4" type="video/mp4" />
+
+                    </video>
+                    <div>
+                        <div className='text_container'>
+                            <p className='content'>Welcome</p>
+                        </div>
+                        <div className='container2'>
+                            <div className='text_container2'>
+                                <p className='content'>Welcome</p>
+                            </div>
+                            <div className='text_container3'>
+                                <p className='content'>Welcome</p>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+
+                </div>
+            </div>
+            <div className='bottom'>
+                <p className='bottom_text'>Home</p>
             </div>
 
 
